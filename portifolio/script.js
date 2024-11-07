43972ee7ea08057de80b303eea28178492f582c3
Mari Bernardes <bernardesramos17@gmail.com>
2024-11-07 14:37:13 -0300

    Add files via upload

---
+++ portifolio/script.js
@@ -0,0 +1,4 @@
+// Exemplo simples de script JavaScript para interatividade (se necessário)
+document.addEventListener('DOMContentLoaded', () => {
+    // Você pode adicionar funcionalidades aqui mais tarde
+});
